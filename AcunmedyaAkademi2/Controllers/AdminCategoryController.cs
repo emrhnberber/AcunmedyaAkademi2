301e636a697636a020581734dfb6c4a123ba1f22
Emirhan Berber <emirhanberber2018@gmail.com>
2025-02-19 17:43:37 +0300

    First Commit

---
+++ AcunmedyaAkademi2/Controllers/AdminCategoryController.cs
@@ -0,0 +1,21 @@
+﻿using AcunmedyaAkademi2.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcunmedyaAkademi2.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        SweetContext context =new SweetContext();
+
+        [Authorize]
+        public ActionResult CategoryList()
+        {
+            var values = context.Categories.ToList();
+            return View();
+        }
+    }
+}
